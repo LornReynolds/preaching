23b4d48e15eb3b496ae7faceac2771c1655f2d19
Lorn <LornReynolds@yahoo.com>
2023-07-18 08:26:23 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,1 @@
-// const API_KEY = "AIzaSyCxz1QK1tKyHAbz7VmW7o7atBNo-Q5XjNg"
-// const API_KEY = "AIzaSyCcGimKs-MDvfPvadwe2-lal1h8pdeEybk"
 const API_KEY = "..."
-
-// API_KEY = "src=https://maps.googleapis.com/maps/api/js?key=AIzaSyC_hnlTSY_hFFSBTGDCcOncRWmX_e_rpGM&callback=initMap"
-
-// API_KEY = "src=https://maps.googleapis.com/maps/api/js?key=AIzaSyCcGimKs-MDvfPvadwe2-lal1h8pdeEybk&callback=initMap"
-// API_KEY = "src=https://maps.googleapis.com/maps/api/js?key=AIzaSyCxz1QK1tKyHAbz7VmW7o7atBNo-Q5XjNg&callback=initMap"
