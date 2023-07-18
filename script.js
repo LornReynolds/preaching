d495fc6cd5532a6ec1729966d1d9746916ee0013
Lorn D. Reynolds <lorn.d.reynolds.civ@us.navy.mil>
2023-07-18 07:45:40 -0700

    pan to location and zoom

--- script.js
+++ script.js
@@ -144,3 +144,14 @@ function initMap() {
         });
       });
     }
+
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(
+        browserHasGeolocation
+          ? "Error: The Geolocation service failed."
+          : "Error: Your browser doesn't support geolocation."
+      );
+      infoWindow.open(map);
+    }
