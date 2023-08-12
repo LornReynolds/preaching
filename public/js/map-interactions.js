59eb107a217f6c312e65180d9f2176603706748e
Lorn <LornReynolds@yahoo.com>
2023-08-12 14:46:41 -0700

    Update map-interactions.js
    Smaller markers, bigger banner

--- public/js/map-interactions.js
+++ public/js/map-interactions.js
@@ -172,7 +172,7 @@ function placeMarker(point) {
         draggable: false,
         icon: {
         url: selected,
-        scaledSize: new google.maps.Size(70, 70),
+        scaledSize: new google.maps.Size(40, 40),
                 },
         scale: 2,
         tilt: 0,
